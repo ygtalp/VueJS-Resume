d9ca37b73b6e1aa77df130956afc676204faef4a
Yiğit Alp Ünal <35925247+ygtalp@users.noreply.github.com>
2019-11-21 12:32:26 -0800

    Add files via upload

---
+++ src/components/experiences.vue
@@ -0,0 +1,176 @@
+<template>
+	<div id="experiences" class="container">
+			<h2>Experiences</h2>
+<p class="lead">
+	“Our true mentor in life is science.”<br>- Atatürk, 1924.
+</p>
+
+<hr>
+
+
+<h3>Educations</h3>
+
+
+<div class="experiences">
+
+					
+	<div class="experience row">
+		<div class="col-md-4">			
+			<h4><a href="https://www.ogu.edu.tr/en" target="_blank">Osmangazi University</a></h4>
+			<p class="experience-period">
+				Sep 2012				- 
+				Aug 2018			</p>
+		</div>
+		<div class="col-md-8">
+			<p>
+				<strong><a href="https://matbil.ogu.edu.tr/Sayfa/Index/32/ders-icerikleri" target="_blank">Bachelor - Mathematics and Computer Science</a></strong>
+				<span class="hidden-phone">
+					The education had the logic of understanding complex algorithm structures with mathematical proofs, so it was math-based education. After the mathematics proficiency, the knowledge of mathematics behind the programming languages ​​was learned. Some of related lessons: <a title="cost calculation, sort algorithm etc." href="https://matbil.ogu.edu.tr/Storage/MatematikBilgisayarBolumu/Uploads/Algoritmalar-TR.pdf" target="_blank">Algorithms</a>, <a title="Ceaser's Cipher, RSA, reverse etc. " href="https://matbil.ogu.edu.tr/Storage/MatematikBilgisayarBolumu/Uploads/Kriptoloji-TR.pdf" target="_blank">Cryptology</a>, <a href="https://matbil.ogu.edu.tr/Storage/MatematikBilgisayarBolumu/Uploads/Oyun-Teori-TR.pdf" target="_blank">Game Theory</a>, <a title="Theory and Haskell Programming Language" href="https://matbil.ogu.edu.tr/Storage/MatematikBilgisayarBolumu/Uploads/Kategori_Tip_ve_Yap%C4%B1lari_TR.pdf" target="_blank">Category</a>, <a href="https://matbil.ogu.edu.tr/Storage/MatematikBilgisayarBolumu/Uploads/Diferensiyel-Geometri-TR.pdf" target="_blank">Differential Geometry</a>, <a href="https://matbil.ogu.edu.tr/Storage/MatematikBilgisayarBolumu/Uploads/Soyut-Cebir-TR.pdf" target="_blank">Abstract Algebra</a>. In terms of programming languages, everything first started by using the basics of simple algorithms on <a title="Project Euler">C++</a> and Matlab. After basic knowledge, I used everything I learned on C# and Java. I've developed a PHP web site which is albums add to archive. After that I used that same idea for .net. Also I learn FileZilla and phpMyAdmin for this projects. Of couse I learned about SQL, CSS, HTML, Bootstrap to create the necessary background. During my time in college; I socialized and met people. Depending on the situation; I studied for weeks and struggle day and night for projects. Of course sometimes we played video games with my homies for days to reward ourselves.	<a title="were great times" >At times like this, our house would suddenly become like game house.</a>	
+									</span>
+				<span class="experience-details">
+					<span class="location">
+						<span class="glyphicon glyphicon-map-marker"></span>
+							<a href="https://www.google.com/search?q=Eskisehir" target="_blank">Eskisehir</a>				</span>
+				</span>
+			</p>
+		</div>
+	</div>
+	
+					
+	<div class="experience row">
+		<div class="col-md-4">
+			<h4>Tekirdag Anadolu Lisesi</h4>
+			<p class="experience-period">
+				Sep 2007				- 
+				Jun 2011			</p>
+		</div>
+		<div class="col-md-8">
+			<p>
+				<strong>Highschool - Science</strong>
+				<span class="hidden-phone">
+					My high school years I educated in science. I have always enjoyed science and worked in this field. In the second year of high school I met delphi by chance and wanted to develop myself in this field. I've been reading articles about the programming languages day and night after that.		</span>
+				<span class="experience-details">
+					<span class="location">
+						<span class="glyphicon glyphicon-map-marker"></span>
+						
+						<a href="https://www.google.com/search?q=tekirdag" target="_blank">Tekirdag</a>				</span>
+				</span>
+			</p>
+		</div>
+	</div>
+	
+					
+
+	
+	
+</div>
+	
+	
+
+<hr>
+
+<h3>Careers</h3>
+
+
+<div class="experiences">
+
+	<div class="experience row">
+		<div class="col-md-4">
+			<h4>Remote</h4>
+			<p class="experience-period">
+				Dec 2018				- 
+				Current		</p>
+		</div>
+		<div class="col-md-8">
+			<p>
+				<strong>Freelancer</strong>
+				<span class="hidden-phone">
+					After graduation, I studied for new technologies. In the meantime, I did my military service. I developed the small-scale sites that come from the internet and currency converter app with react native. I still reading the documentation and following new technologies and take time for myself.				</span>
+				<span class="experience-details">
+					<span class="location">
+						<span class="glyphicon glyphicon-map-marker"></span>
+						<a href="https://www.google.com/search?q=istanbul" target="_blank">Istanbul</a>	</span>
+					
+										
+					<span class="seperator">|</span>
+					<span class="link">
+						<span class="glyphicon glyphicon-link"></span>
+						<a href="http://www.dummpster.com" target="_blank">dummpster.com</a>
+					</span>
+					
+										
+				</span>
+			</p>
+		</div>
+	</div>
+
+	<div class="experience row">
+		<div class="col-md-4">
+			<h4>İyi Fikir İnteraktif Teknolojiler</h4>
+			<p class="experience-period">
+				Sep 2016				- 
+				Nov 2018		</p>
+		</div>
+		<div class="col-md-8">
+			<p>
+				<strong>Fulltime - Developer</strong>
+				<span class="hidden-phone">
+					iyifikir is a company that develops applications for municipal institutions. I worked in that company to develop mobile applications by using React and also joined web projects when its necessary.				</span>
+				<span class="experience-details">
+					<span class="location">
+						<span class="glyphicon glyphicon-map-marker"></span>
+						<a href="https://www.google.com/search?q=eskisehir" target="_blank">Eskisehir</a>	</span>
+					
+										
+					<span class="seperator">|</span>
+					<span class="link">
+						<span class="glyphicon glyphicon-link"></span>
+						<a href="http://www.iyifikirlabs.com/" target="_blank">iyifikirlabs.com</a>
+					</span>
+					
+										
+				</span>
+			</p>
+		</div>
+	</div>
+
+
+
+	<div class="experience row">
+		<div class="col-md-4">
+			<h4>Efe Eroglu Bilişim Teknolojileri | Health Informatics</h4>
+			<p class="experience-period">
+				Jan 2018				- 
+				Feb 2018		</p>
+		</div>
+		<div class="col-md-8">
+			<p>
+				<strong>Internship</strong>
+				<span class="hidden-phone">
+					As interns we have written console applications in java and then unit tests of these console applications. applications go from simple to difficult. Prime numbers from 1 to 100, the first 20 Fibonacci series, the first ten-stage pascal triangle, hanoi towers, eight queen, randomly distributed playing cards for four people, garage application, etc.			</span>
+				<span class="experience-details">
+					<span class="location">
+						<span class="glyphicon glyphicon-map-marker"></span>
+						<a href="https://www.google.com/search?q=eskisehir" target="_blank">Eskisehir</a>		</span>
+					
+										
+					<span class="seperator">|</span>
+					<span class="link">
+						<span class="glyphicon glyphicon-link"></span>
+						<a href="http://www.enlil.com.tr/" target="_blank">http://www.enlil.com.tr/</a>
+					</span>							
+				</span>
+			</p>
+		</div>
+	</div>
+	
+</div>
+	
+		</div>
+</template>
+
+<style>
+	a{
+		color:#434242;
+	}
+</style>
